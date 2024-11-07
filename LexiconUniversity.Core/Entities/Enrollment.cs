b4cf6f8b1945c9cbd9ca15d3fe8f6f850b58dcb7
Jonathan Krall <jonathan.krall@lexicon.se>
2024-11-07 11:51:13 +0100

    Added Entities

---
+++ LexiconUniversity.Core/Entities/Enrollment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconUniversity.Core.Entities
+{
+    public class Enrollment
+    {
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+
+        public int Grade { get; set; }
+    }
+}
