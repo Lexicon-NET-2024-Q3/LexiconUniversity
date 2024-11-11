b5d96639598dd356f3717587a83311c219d7fa55
Jonathan Krall <jonathan.krall@lexicon.se>
2024-11-11 11:59:23 +0100

    StreetNr-validation

--- LexiconUniversity.Web/Models/ViewModels/StudentCreateViewModel.cs
+++ LexiconUniversity.Web/Models/ViewModels/StudentCreateViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace LexiconUniversity.Web.Models.ViewModels
+﻿using LexiconUniversity.Web.Validations;
+
+namespace LexiconUniversity.Web.Models.ViewModels
 {
     public class StudentCreateViewModel
     {
         public string NameFirstName { get; set; }
         public string NameLastName { get; set; }
         public string Email { get; set; }
+        [CheckStreetNr(10)]
         public string AddressStreet { get; set; }
         public string AddressZipCode { get; set; }
         public string AddressCity { get; set; }
---
+++ LexiconUniversity.Web/Validations/CheckStreetNr.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.ComponentModel.DataAnnotations;
+
+namespace LexiconUniversity.Web.Validations
+{
+    public class CheckStreetNr : ValidationAttribute
+    {
+        private readonly int max;
+
+        public CheckStreetNr(int max)
+        {
+            this.max = max;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if(value is string input)
+            {
+                var num = input.Trim().Split().Last();
+                return int.TryParse(num, out int res) & res <= max;
+            }
+            return false; 
+        }
+    }
+}
