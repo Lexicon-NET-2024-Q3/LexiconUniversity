770530c2613da64aac7aea4142ded21e4230f7a4
Jonathan Krall <jonathan.krall@lexicon.se>
2024-11-11 13:27:56 +0100

    Added FrontEnd-validation.

--- LexiconUniversity.Web/Validations/CheckStreetNr.cs
+++ LexiconUniversity.Web/Validations/CheckStreetNr.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.ComponentModel.DataAnnotations;
 
 namespace LexiconUniversity.Web.Validations
 {
-    public class CheckStreetNr : ValidationAttribute
+    public class CheckStreetNr : ValidationAttribute, IClientModelValidator
     {
         private readonly int max;
 
         public CheckStreetNr(int max)
         {
             this.max = max;
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            context.Attributes.Add("data-val", "true");
+            context.Attributes.Add("data-val-streetnr", "Not a valid street nr");
+            context.Attributes.Add("data-val-streetnr-max", $"{max}");
         }
 
         public override bool IsValid(object? value)
